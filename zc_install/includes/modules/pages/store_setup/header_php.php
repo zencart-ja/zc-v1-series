6e9bf253bd9f856ba649ac0c3b4e5bf5179a7b93
yama <yamamoto@kyms.ne.jp>
2013-01-14 17:11:06 +0900

    インストール時のデフォルトの国・store_zone・通貨を日本・東京・円にセット

--- zc_install/includes/modules/pages/store_setup/header_php.php
+++ zc_install/includes/modules/pages/store_setup/header_php.php
@@ -14,11 +14,11 @@
   if (!isset($_POST['store_name'])) $_POST['store_name'] = '';
   if (!isset($_POST['store_owner'])) $_POST['store_owner'] = '';
   if (!isset($_POST['store_owner_email'])) $_POST['store_owner_email'] = '';
-  if (!isset($_POST['store_country'])) $_POST['store_country'] = '223';
-  if (!isset($_POST['store_zone'])) $_POST['store_zone'] = '';
+  if (!isset($_POST['store_country'])) $_POST['store_country'] = '107';
+  if (!isset($_POST['store_zone'])) $_POST['store_zone'] = '312';
   if (!isset($_POST['store_address'])) $_POST['store_address'] = STORE_ADDRESS_DEFAULT_VALUE;
-  if (!isset($_POST['store_default_language'])) $_POST['store_default_language'] = '';
-  if (!isset($_POST['store_default_currency'])) $_POST['store_default_currency'] = '';
+  if (!isset($_POST['store_default_language'])) $_POST['store_default_language'] = 'ja';
+  if (!isset($_POST['store_default_currency'])) $_POST['store_default_currency'] = 'JPY';
 
   @require_once('../includes/configure.php');
   if (!defined('DB_TYPE') || DB_TYPE=='') {
