477b6382455ca320e8b812d80d44a5089e9fed18
kimono <maeda@obitastar.co.jp>
2014-07-26 17:10:29 +0900

    文字コードが間違っていたため、再度修正。
    Signed-off-by: kimono <maeda@obitastar.co.jp>

--- includes/extra_configures/security_patch_v138_20080919.php
+++ includes/extra_configures/security_patch_v138_20080919.php
@@ -57,9 +57,9 @@ function securityPatchSanitizePostVariableProductsId ($arrayToSanitize)
   foreach ($arrayToSanitize as $key => $variableToSanitize)
   {
     {
-      $arrayToSanitize[$key] = preg_replace('/[^0-9a-fA-FÇO-ÇX:.]/', '', $variableToSanitize);
+      $arrayToSanitize[$key] = preg_replace('/[^0-9a-fA-FÔºê-Ôºô:.]/', '', $variableToSanitize);
     }
-    if (preg_replace('/[0-9a-zA-zÇO-ÇX_:.]/', '', $key) != '')
+    if (preg_replace('/[0-9a-zA-zÔºê-Ôºô_:.]/', '', $key) != '')
       unset($arrayToSanitize[$key]);
   }
   return $arrayToSanitize;
