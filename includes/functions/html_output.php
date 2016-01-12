51d83935e0a29ebc9778455a527f77be38600c2d
kimono <maeda@obitastar.co.jp>
2016-01-12 13:59:01 +0900

    本家の「Security Patches for v1.5.4 - November 2015」にて修正されたセキュリティパッチ
    Signed-off-by: kimono <maeda@obitastar.co.jp>

--- includes/functions/html_output.php
+++ includes/functions/html_output.php
@@ -4,10 +4,10 @@
  * HTML-generating functions used throughout the core
  *
  * @package functions
- * @copyright Copyright 2003-2011 Zen Cart Development Team
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: html_output.php 19355 2011-08-21 21:12:09Z drbyte $
+ * @version GIT: $Id: Author: DrByte  Modified in v1.5.5 $
  */
 
 /*
@@ -386,7 +386,7 @@
  *  Output a form password field
  */
   function zen_draw_password_field($name, $value = '', $parameters = 'maxlength="40"') {
-    return zen_draw_input_field($name, $value, $parameters, 'password', true);
+    return zen_draw_input_field($name, $value, $parameters, 'password', false);
   }
 
 /*
